50e689ca2d3d0ea60200ec2c38052a7b717a6c54
naisu <napas14204@gmail.com>
2023-09-07 00:44:30 +0700

    add type check

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { log } from "console";
 import fs from "fs";
 import path from "path";
 import readline from "readline";
 
-let directoryPath = "./images/";
+let directoryPath: string = "./images/";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,18 +14,21 @@ if (!fs.existsSync(directoryPath)) {
   fs.mkdirSync(directoryPath);
 }
 
-function promptToContinue() {
-  rl.question("Do you want to download another book? (y/n): ", (answer) => {
-    if (answer.toLowerCase() === "y") {
-      startDownload();
-    } else {
-      rl.close();
+function promptToContinue(): void {
+  rl.question(
+    "Do you want to download another book? (y/n): ",
+    (answer: string) => {
+      if (answer.toLowerCase() === "y") {
+        startDownload();
+      } else {
+        rl.close();
+      }
     }
-  });
+  );
 }
 
-function startDownload() {
-  rl.question("Enter the book directory ID: ", (dirId) => {
+function startDownload(): void {
+  rl.question("Enter the book directory ID: ", (dirId: string) => {
     if (dirId.length > 5) {
       console.log(
         "Invalid directory ID. It should not have more than five characters."
@@ -35,43 +37,46 @@ function startDownload() {
       return;
     }
 
-    rl.question("Enter the directory name you want to create: ", (dirName) => {
-      dirName = dirName.replace(/[^a-zA-Z0-9]/g, "");
+    rl.question(
+      "Enter the directory name you want to create: ",
+      (dirName: string) => {
+        dirName = dirName.replace(/[^a-zA-Z0-9]/g, "");
 
-      directoryPath += dirName;
+        directoryPath += dirName;
 
-      if (!fs.existsSync(directoryPath)) {
-        fs.mkdirSync(directoryPath);
-      }
-      let i = 1;
-      let fetch = true;
-
-      const fetchPage = async () => {
-        try {
-          if (!fetch) {
-            promptToContinue();
-            return;
-          }
-          const response = await axios.get(
-            `http://readonline.ebookstou.org/flipbook/${dirId}/files/mobile/${i}.jpg`,
-            { responseType: "arraybuffer" }
-          );
-
-          if (response.status === 200) {
-            const filePath = path.join(directoryPath, `${i}.jpg`);
-            fs.writeFileSync(filePath, response.data, "binary");
-            console.log(`Downloaded ${filePath}`);
-            i++;
-            fetchPage();
-          }
-        } catch (error) {
-          fetch = false;
-          console.log("Ended of downloading process.");
-          promptToContinue();
+        if (!fs.existsSync(directoryPath)) {
+          fs.mkdirSync(directoryPath);
         }
-      };
-      fetchPage();
-    });
+        let i: number = 1;
+        let fetch: boolean = true;
+
+        const fetchPage = async (): Promise<void> => {
+          try {
+            if (!fetch) {
+              promptToContinue();
+              return;
+            }
+            const response = await axios.get(
+              `http://readonline.ebookstou.org/flipbook/${dirId}/files/mobile/${i}.jpg`,
+              { responseType: "arraybuffer" }
+            );
+
+            if (response.status === 200) {
+              const filePath = path.join(directoryPath, `${i}.jpg`);
+              fs.writeFileSync(filePath, response.data, "binary");
+              console.log(`Downloaded ${filePath}`);
+              i++;
+              fetchPage();
+            }
+          } catch (error) {
+            fetch = false;
+            console.log("Ended of downloading process.");
+            promptToContinue();
+          }
+        };
+        fetchPage();
+      }
+    );
   });
 }
 
